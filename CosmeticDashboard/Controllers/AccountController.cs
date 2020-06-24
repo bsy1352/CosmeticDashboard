5d3c4cdab2a15842b10f2266d2b33905afdbfbf0
Si-Yoon Bong <bong@granco.co.kr>
2020-06-24 18:52:12 +0900

    1. 팝업 모달 픽스 2. 로그인창 새로운 UI 3. 회원가입창 새로운 UI (진행중)

--- CosmeticDashboard/Controllers/AccountController.cs
+++ CosmeticDashboard/Controllers/AccountController.cs
@@ -27,12 +27,53 @@ namespace CosmeticDashboard.Controllers
         /// 로그인
         /// </summary>
         /// <returns></returns>
+        
         [HttpGet]
-        public IActionResult Login()
+        public IActionResult LoginPage()
         {
-            
+
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> LoginPage(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                // Linq 사용
+                // => 람다식 ; A Go to B
+                // == -> 사용자제
+                //await _db.Users.FirstOrDefaultAsync(u => u.UserId == model.UserId && u.UserPassword == model.UserPassword);
+
+                var user = await _db.Users
+                    .FirstOrDefaultAsync(u => u.UserId.Equals(model.UserId) &&
+                                                                   u.UserPassword.Equals(model.UserPassword));
+
+                //회원가입 아이디 중복체크 시에 사용한다
+                //악의적인 방법을 막기위해
+                //var user = await _db.Users.FirstOrDefaultAsync(u => u.UserId.Equals(model.UserId));
+
+                if (user != null)
+                {
+
+                    // key 값과 value값을 필요로 한다
+                    HttpContext.Session.SetInt32("USER_LOGIN_KEY", user.UserNo);
+                    HttpContext.Session.SetString("USER_LOGIN_ID", user.UserId);
+                    HttpContext.Session.SetString("USER_LOGIN_NAME", user.UserName);
+                    //로그인에 성공했을 때
+                    return RedirectToAction("Index", "Home");
+
+                }
+
+                TempData["LoginFailure"] = "로그인실패";
+            }
+
+            return View();
+        }
+
+     
 
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel model)
@@ -68,14 +109,53 @@ namespace CosmeticDashboard.Controllers
                     
                 }
 
-                //TempData["LoginFailure"] = "로그인실패";
-                //RequestHeaders header = Request.GetTypedHeaders();
-                //return new PartialViewResult();
-                return View(model);
+                TempData["LoginFailure"] = "로그인실패";
             }
 
             return View(model);
         }
+
+        //public async Task<JsonResult> LoginJason()
+        //{
+
+
+        //    if (ModelState.IsValid)
+        //    {
+
+
+        //        Linq 사용
+        //         => 람다식; A Go to B
+        //         == ->사용자제
+        //        await _db.Users.FirstOrDefaultAsync(u => u.UserId == model.UserId && u.UserPassword == model.UserPassword);
+
+        //        var user = await _db.Users
+        //            .FirstOrDefaultAsync(u => u.UserId.Equals(model.UserId) &&
+        //                                                           u.UserPassword.Equals(model.UserPassword));
+
+        //        회원가입 아이디 중복체크 시에 사용한다
+        //        악의적인 방법을 막기위해
+        //        var user = await _db.Users.FirstOrDefaultAsync(u => u.UserId.Equals(model.UserId));
+
+        //        if (user != null)
+        //        {
+
+        //            key 값과 value값을 필요로 한다
+        //            HttpContext.Session.SetInt32("USER_LOGIN_KEY", user.UserNo);
+        //            HttpContext.Session.SetString("USER_LOGIN_ID", user.UserId);
+        //            HttpContext.Session.SetString("USER_LOGIN_NAME", user.UserName);
+        //            로그인에 성공했을 때
+        //            return RedirectToAction("Index", "Home");
+
+        //        }
+
+        //        ViewBag.Error = "fsdf";
+        //        RequestHeaders header = Request.GetTypedHeaders();
+        //        return new PartialViewResult();
+        //        return Json(model);
+        //    }
+
+        //    return Json(model);
+        //}
 
         public async Task<IActionResult> Logout()
         {
