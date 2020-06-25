465e72b20769d14b7659d520cb82ca51f000ba47
Si-Yoon Bong <bong@granco.co.kr>
2020-06-25 18:53:00 +0900

    1. 회원가입 UI 추가

--- CosmeticDashboard/Controllers/AccountController.cs
+++ CosmeticDashboard/Controllers/AccountController.cs
@@ -35,6 +35,7 @@ namespace CosmeticDashboard.Controllers
             return View();
         }
 
+        
         [HttpPost]
         public async Task<IActionResult> LoginPage(LoginViewModel model)
         {
@@ -73,89 +74,22 @@ namespace CosmeticDashboard.Controllers
             return View();
         }
 
-     
 
-        [HttpPost]
-        public async Task<IActionResult> Login(LoginViewModel model)
+        [HttpGet]
+        public IActionResult Registration()
         {
-            
 
-            if (ModelState.IsValid)
-            {
-                
-
-                // Linq 사용
-                // => 람다식 ; A Go to B
-                // == -> 사용자제
-                //await _db.Users.FirstOrDefaultAsync(u => u.UserId == model.UserId && u.UserPassword == model.UserPassword);
-
-                var user = await _db.Users
-                    .FirstOrDefaultAsync(u => u.UserId.Equals(model.UserId) &&
-                                                                   u.UserPassword.Equals(model.UserPassword));
-
-                //회원가입 아이디 중복체크 시에 사용한다
-                //악의적인 방법을 막기위해
-                //var user = await _db.Users.FirstOrDefaultAsync(u => u.UserId.Equals(model.UserId));
-
-                if (user != null)
-                {
-                    
-                    // key 값과 value값을 필요로 한다
-                    HttpContext.Session.SetInt32("USER_LOGIN_KEY",  user.UserNo);
-                    HttpContext.Session.SetString("USER_LOGIN_ID", user.UserId);
-                    HttpContext.Session.SetString("USER_LOGIN_NAME", user.UserName);
-                    //로그인에 성공했을 때
-                    return RedirectToAction("Index", "Home");
-                    
-                }
-
-                TempData["LoginFailure"] = "로그인실패";
-            }
-
-            return View(model);
+            return View();
         }
 
-        //public async Task<JsonResult> LoginJason()
-        //{
+        [HttpPost]
+        public async Task<IActionResult> Registration(LoginViewModel model)
+        {
+
+            return View();
+        }
 
 
-        //    if (ModelState.IsValid)
-        //    {
-
-
-        //        Linq 사용
-        //         => 람다식; A Go to B
-        //         == ->사용자제
-        //        await _db.Users.FirstOrDefaultAsync(u => u.UserId == model.UserId && u.UserPassword == model.UserPassword);
-
-        //        var user = await _db.Users
-        //            .FirstOrDefaultAsync(u => u.UserId.Equals(model.UserId) &&
-        //                                                           u.UserPassword.Equals(model.UserPassword));
-
-        //        회원가입 아이디 중복체크 시에 사용한다
-        //        악의적인 방법을 막기위해
-        //        var user = await _db.Users.FirstOrDefaultAsync(u => u.UserId.Equals(model.UserId));
-
-        //        if (user != null)
-        //        {
-
-        //            key 값과 value값을 필요로 한다
-        //            HttpContext.Session.SetInt32("USER_LOGIN_KEY", user.UserNo);
-        //            HttpContext.Session.SetString("USER_LOGIN_ID", user.UserId);
-        //            HttpContext.Session.SetString("USER_LOGIN_NAME", user.UserName);
-        //            로그인에 성공했을 때
-        //            return RedirectToAction("Index", "Home");
-
-        //        }
-
-        //        ViewBag.Error = "fsdf";
-        //        RequestHeaders header = Request.GetTypedHeaders();
-        //        return new PartialViewResult();
-        //        return Json(model);
-        //    }
-
-        //    return Json(model);
-        //}
 
         public async Task<IActionResult> Logout()
         {
@@ -167,38 +101,19 @@ namespace CosmeticDashboard.Controllers
             return RedirectToAction("Index", "Home");
         }
 
-        /// <summary>
-        /// 회원가입
-        /// </summary>
-        /// <returns></returns>
-        public IActionResult Register()
-        {
-            return View();
-        }
-
-        /// <summary>
-        /// 회원 가입 전송
-        /// </summary>
-        /// <param name="model"></param>
-        /// <returns></returns>
-        [HttpPost]
-        public async Task<IActionResult> Register(User model)
-        {
-            if (ModelState.IsValid)
-            {
-                await _db.Users.AddAsync(model);
-                await _db.SaveChangesAsync();
-                return RedirectToAction("Index", "Home");
-            }
-
-            return View(model);
-        }
-
-     
-        public async Task<PartialViewResult> LoginFailure()
+        public IActionResult LoginFailure()
         {
 
             return PartialView();
+        }
+
+        [HttpPost]
+        public IActionResult isDuplicated(Test test)
+        {
+            Console.WriteLine(test.data);
+            Console.WriteLine(test.foo);
+            Console.WriteLine(test.fooz);
+            return View();
         }
     }
 }
---
+++ CosmeticDashboard/Models/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CosmeticDashboard.Models
+{
+    public class Test
+    {
+        public string data { get; set; }
+        /// <summary>
+        /// 사용자 이름
+        /// </summary>
+        public string foo { get; set; }
+
+        /// <summary>
+        /// 사용자 아이디
+        /// </summary>
+        public string fooz { get; set; }
+    }
+}
