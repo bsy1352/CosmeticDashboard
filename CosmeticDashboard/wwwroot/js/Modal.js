11e88ab5a671dfba4d66e8c036bb140442e7209a
Si-Yoon Bong <bong@granco.co.kr>
2020-08-10 18:57:12 +0900

    26

---
+++ CosmeticDashboard/wwwroot/js/Modal.js
@@ -0,0 +1,40 @@
+﻿
+
+function popUpmodal1() {
+    
+    blockOverflow()
+    $('.modal-bg').addClass('bg-active');
+
+    $('.modal_1').addClass('md-active');
+    
+  
+}
+
+function popUpmodal2() {
+
+    blockOverflow()
+    $('.modal-bg').addClass('bg-active');
+
+    $('.modal_2').addClass('md-active');
+        
+
+
+
+}
+
+function blockOverflow() {
+    $('body').addClass('noscrolling');
+}
+
+function startOverflow() {
+    $('body').removeClass('noscrolling');
+}
+
+
+function popupClose() {
+    startOverflow();
+    $('.modal-bg').removeClass('bg-active');
+    $('.modal_1').removeClass('md-active');
+    $('.modal_2').removeClass('md-active');
+}
+
